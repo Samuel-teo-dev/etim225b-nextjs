56c59080fa1fdb5fcfc15fe09f7610960dc417f5
SAMUEL TEODORO SILVA <samuel.silva859>
2026-03-16 15:20:03 -0300

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+import Link from "next/Link";
+
 import styles from "./page.module.css";
 
 function Home() {
   return (
 <div className={styles.containerHome}>
+  <Link href="/Exemplos/Ex-01">Exemplo 1</Link>
 <div className={styles.containerListas}>
 <h1>Exemplos</h1>
 </div>
